9dfb2eeebdb37ab3489e8525a98bf66d4a601caa
John Salas <salas.john@hotmail.com>
2023-08-01 00:16:39 -0500

    End app with band backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,45 @@
 const { io } = require('../index');
 
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
 // Socket messages
 io.on('connection', (client) => {
+    console.log('Client connected');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('event', (data) => {
         console.log('Connected client');
     });
     
     client.on('disconnect', () => {
-        console.log('Desconected client');
+        console.log('Disconnected client');
     });
 
-    client.on('sendMessage', (data, callback) => {
-        console.log('data', data);
+    client.on('sendMessage', (payload) => {
+        io.emit('sendMessage', payload);
     });
+    
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+    
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    
 });
